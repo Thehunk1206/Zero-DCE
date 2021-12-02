d0a96736c8761b46b09261ec057bf94a01d0f5bc
thehunk1206 <khantauhid313@gmail.com>
2021-12-02 20:53:35 +0530

    loss funtion implemented. Ready to be tested

---
+++ ZeroDCE/losses.py
@@ -0,0 +1,154 @@
+'''
+MIT License
+
+Copyright (c) 2021 Tauhid Khan
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+'''
+import os 
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
+import tensorflow as tf
+
+class SpatialConsistencyLoss(tf.keras.losses.Loss):
+    '''
+    The spatial consistency loss encourages spatial coherence of the enhanced image through
+    preserving the difference of neighboring regions between the input
+    image and its enhanced version.
+
+    Highly Refereced from the orignal Pytorch Implementation of the ZeroDCE:
+    https://github.com/Li-Chongyi/Zero-DCE_extension
+    '''
+    def __init__(self, **kwargs):
+        super(SpatialConsistencyLoss, self).__init__(**kwargs)
+        self.kernel_left    = tf.constant([[[[0, 0, 0]], [[-1, 1, 0]], [[0, 0, 0]]]], dtype=tf.float32)
+        self.kernel_right   = tf.constant([[[[0, 0, 0]], [[0, 1, -1]], [[0, 0, 0]]]], dtype=tf.float32)
+        self.kernel_up      = tf.constant([[[[0, -1, 0]], [[0, 1, 0]], [[0, 0, 0]]]], dtype=tf.float32)
+        self.kernel_down    = tf.constant([[[[0, 0, 0]], [[0, 1, 0]], [[0, -1, 0]]]], dtype=tf.float32)
+
+    def call(self, inp_image: tf.Tensor, enh_image: tf.Tensor)->tf.Tensor:
+        '''
+        Args:
+            inp_image: The input image tensor.
+            enh_image: The Low Light Enhanced image tensor.
+        Returns:
+            The spatial consistency loss.
+        '''
+        inp_image_mean = tf.reduce_mean(inp_image, axis=-1, keepdims=True)
+        enh_image_mean = tf.reduce_mean(enh_image, axis=-1, keepdims=True)
+
+        inp_image_pooled = tf.nn.avg_pool2d(inp_image_mean, ksize=4, strides=4, padding='VALID')
+        enh_image_pooled = tf.nn.avg_pool2d(enh_image_mean, ksize=4, strides=4, padding='VALID')
+
+        D_inp_left  = tf.nn.conv2d(inp_image_pooled, self.kernel_left, strides=[1, 1, 1, 1], padding='SAME')
+        D_inp_right = tf.nn.conv2d(inp_image_pooled, self.kernel_right, strides=[1, 1, 1, 1], padding='SAME')
+        D_inp_up    = tf.nn.conv2d(inp_image_pooled, self.kernel_up, strides=[1, 1, 1, 1], padding='SAME')
+        D_inp_down  = tf.nn.conv2d(inp_image_pooled, self.kernel_down, strides=[1, 1, 1, 1], padding='SAME')
+
+        D_enh_left  = tf.nn.conv2d(enh_image_pooled, self.kernel_left, strides=[1, 1, 1, 1], padding='SAME')
+        D_enh_right = tf.nn.conv2d(enh_image_pooled, self.kernel_right, strides=[1, 1, 1, 1], padding='SAME')
+        D_enh_up    = tf.nn.conv2d(enh_image_pooled, self.kernel_up, strides=[1, 1, 1, 1], padding='SAME')
+        D_enh_down  = tf.nn.conv2d(enh_image_pooled, self.kernel_down, strides=[1, 1, 1, 1], padding='SAME')
+
+        D_left  = tf.square(D_inp_left - D_enh_left)
+        D_right = tf.square(D_inp_right - D_enh_right)
+        D_up    = tf.square(D_inp_up - D_enh_up)
+        D_down  = tf.square(D_inp_down - D_enh_down)
+
+        return tf.reduce_mean(D_left + D_right + D_up + D_down)
+    
+    def get_config(self):
+        return super(SpatialConsistencyLoss, self).get_config()
+    
+    @classmethod
+    def from_config(cls, config):
+        return cls(**config)
+
+class ExposureControlLoss(tf.keras.losses.Loss):
+    '''
+    To restrain under-/over-exposed regions,
+    The authors of the paper designed an exposure control loss to control the exposure
+    level. The exposure control loss measures the distance between the
+    average intensity value of a local region to the well-exposedness
+    level E. The value of E is emperically set to 0.6 in the paper
+    '''
+    def __init__(self, e:float = 0.6, patch_size: int = 16, **kwargs):
+        super(ExposureControlLoss, self).__init__(**kwargs)
+        assert e > 0.0 and e < 1.0, 'The Value of e must be between 0.0 and 1.0'
+
+        self.e = e
+        self.patch_size = patch_size
+
+    def __call__(self, enhanced_img: tf.Tensor)-> tf.Tensor:
+        '''
+        Args:
+            enhanced_img: The Low Light Enhanced image tensor.
+        Returns:
+            The exposure control loss.
+        '''
+        enhanced_img        = tf.reduce_mean(enhanced_img, axis=-1, keepdims=True)
+        enhanced_img_pooled = tf.nn.avg_pool2d(enhanced_img, ksize=self.patch_size, strides=self.patch_size, padding='VALID')
+
+        D = tf.reduce_mean(tf.square(enhanced_img_pooled - self.e))
+        return D
+    
+    def get_config(self):
+        config = super(ExposureControlLoss, self).get_config()
+        config.update({
+            'e': self.e,
+            'patch_size': self.patch_size
+        })
+        return config
+    
+    @classmethod
+    def from_config(cls, config):
+        return cls(**config)
+
+class ColorConstancyLosss(tf.keras.losses.Loss):
+    '''
+    By Following the Gray-World color constancy hypothesis that color in each sensor 
+    channel averages to gray over the entire image, the authors of this paper designed 
+    a color constancy loss to correct the potential color deviations in the enhanced 
+    image and also build the relations among the three adjusted channels.
+    '''
+    def __init__(self, **kwargs):
+        super(ColorConstancyLosss, self).__init__(**kwargs)
+    
+    def __call__(self, enhanced_img: tf.Tensor)-> tf.Tensor:
+        '''
+        Args:
+            enhanced_img: The Low Light Enhanced image tensor.
+        Returns:
+            The color constancy loss.
+        '''
+        mean_rgb = tf.reduce_mean(enhanced_img, axis=(1,2), keepdims=True)
+        mr, mg, mb = tf.split(mean_rgb, 3, axis=-1)
+        Drg = tf.square(mr - mg)
+        Drb = tf.square(mr - mb)
+        Dbg = tf.square(mb - mg)
+
+        return tf.sqrt(tf.square(Drg) + tf.square(Drb) + tf.square(Dbg))
+    
+    def get_config(self):
+        return super(ColorConstancyLosss, self).get_config()
+    
+    @classmethod
+    def from_config(cls, config):
+        return cls(**config)
+
--- ZeroDCE/zero_dce.py
+++ ZeroDCE/zero_dce.py
@@ -1,3 +1,27 @@
+'''
+MIT License
+
+Copyright (c) 2021 Tauhid Khan
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+'''
+
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
@@ -6,10 +30,13 @@ import tensorflow as tf
 
 class ZeroDCE(tf.keras.Model):
 
-    def __init__(self, filters:int = 32, iteration: int = 8):
-        super(ZeroDCE, self).__init__()
+    def __init__(self, name:str = "DCE-net", filters:int = 32, iteration: int = 8, IMG_H:int = 384, IMG_W:int =512, IMG_C:int = 3, **kwargs):
+        super(ZeroDCE, self).__init__(name=name, **kwargs)
         self.filters = filters
         self.iteration = iteration
+        self.IMG_H = IMG_H
+        self.IMG_W = IMG_W
+        self.IMG_C = IMG_C
 
         self.conv1 = tf.keras.layers.Conv2D(self.filters, kernel_size=(3, 3), strides=(1,1), padding='same',activation='relu')
         self.conv2 = tf.keras.layers.Conv2D(self.filters, kernel_size=(3, 3), strides=(1,1), padding='same',activation='relu')
@@ -20,7 +47,7 @@ class ZeroDCE(tf.keras.Model):
         self.concat_2_5 = tf.keras.layers.Concatenate(axis=-1)
         self.conv6 = tf.keras.layers.Conv2D(self.filters, kernel_size=(3, 3), strides=(1,1), padding='same',activation='relu')
         self.concat_1_6 = tf.keras.layers.Concatenate(axis=-1)
-        self.a_map_conv = tf.keras.layers.Conv2D(self.iteration*3, kernel_size=(3, 3), strides=(1,1), padding='same',activation='relu')
+        self.a_map_conv = tf.keras.layers.Conv2D(self.iteration*3, kernel_size=(3, 3), strides=(1,1), padding='same',activation='tanh')
     
     def call(self, inputs: tf.Tensor):
         x1 = self.conv1(inputs)
@@ -33,15 +60,19 @@ class ZeroDCE(tf.keras.Model):
         x_6 = self.conv6(x_concat_2_5)
         x_concat_1_6 = self.concat_1_6([x1, x_6])
         a_maps = self.a_map_conv(x_concat_1_6)
+
+        # Enhancing input image iteratively 
         a_maps_splited = tf.split(a_maps, self.iteration, axis=-1)
         le_img = inputs
         for a_map in a_maps_splited:
             le_img = le_img + a_map * (tf.square(le_img) - le_img)
         return le_img
 
-    def summary(self):
-        x = tf.keras.Input(shape=(400, 600, 3))
-        model = tf.keras.Model(inputs=[x], outputs=self.call(x), name='Unet3D')
+    def summary(self, plot:bool = False):
+        x = tf.keras.Input(shape=(self.IMG_H, self.IMG_W, self.IMG_C))
+        model = tf.keras.Model(inputs=[x], outputs=self.call(x), name='DCE-net')
+        if plot:
+            tf.keras.utils.plot_model(model, to_file='DCE-net.png', show_shapes=True, show_layer_names=True, rankdir='TB')
         return model.summary()
     
     def get_config(self):
@@ -58,7 +89,7 @@ class ZeroDCE(tf.keras.Model):
 if __name__ == "__main__":
     x = tf.random.normal([1, 400, 600, 3])
     model = ZeroDCE(filters=32, iteration=5)
-    tf.print(model.summary())
+    tf.print(model.summary(plot=True))
     tf.print(model.get_config())
     y = model(x)
-    print(y.shape)
+    tf.print(y.shape)
